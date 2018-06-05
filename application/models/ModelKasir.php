a8d90a0b9569a19f74292ffb9b1b68c37f41cc6a
shrkboy <ilhamn33@gmail.com>
2018-06-06 00:31:59 +0700

    model for data_kasir

--- application/models/ModelKasir.php
+++ application/models/ModelKasir.php
@@ -8,5 +8,30 @@
 
 class ModelKasir extends CI_Model
 {
+    public $id_kasir;
+    public $username;
+    public $hash_password;
 
+       function getAll(){
+        $query = $this->db->get('data_kasir');
+        return $query->result();
+    }
+
+    function getbyId($data){
+        $query = $this->db->get_where('data_kasir', array('id_kasir' => $data));
+        return $query->row();
+    }
+
+    public function login($id,$pass){
+
+        $query = $this->db->get_where('data_kasir', array('id_kasir' => $id, 'hash_password' => $pass));
+
+        if($query->result())
+        {
+            return $query->row();
+        }
+        else{
+            return false;
+        }
+    }
 }
