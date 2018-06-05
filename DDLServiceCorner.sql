fd41e94eb92ebeada74c3561f11eb0b8bf5ed5d0
Ilham Nurfathiya <ilhamn33@gmail.com>
2018-06-05 23:27:48 +0700

    Update DDL

--- DDLServiceCorner.sql
+++ DDLServiceCorner.sql
@@ -10,10 +10,10 @@ create table data_kasir
 
 create table data_layanan
 (
-  id_pelayanan varchar(5)  not null
+  id_layanan varchar(5)  not null
     primary key,
-  nama         varchar(30) not null,
-  harga        int         not null
+  nama       varchar(30) not null,
+  harga      int         not null
 );
 
 create table data_pelanggan
@@ -36,9 +36,9 @@ create table data_sparepart
   harga   int         not null
 );
 
-create table data_transaksi
+create table data_transaksi_master
 (
-  id_transaksi  int auto_increment
+  id_master     int auto_increment
     primary key,
   tanggal       date not null,
   id_pelanggan  int  not null,
@@ -51,10 +51,49 @@ create table data_transaksi
   foreign key (id_kasir) references data_kasir (id_kasir)
 );
 
+create table data_transaksi_layanan
+(
+  id_transaksi int auto_increment
+    primary key,
+  id_master    int        not null,
+  id_layanan   varchar(5) not null,
+  total_biaya  int        not null,
+  constraint data_transaksi_layanan_data_transaksi_master_id_master_fk
+  foreign key (id_master) references data_transaksi_master (id_master),
+  constraint data_transaksi_layanan_data_layanan_id_layanan_fk
+  foreign key (id_layanan) references data_layanan (id_layanan)
+);
+
+create index data_transaksi_layanan_data_layanan_id_layanan_fk
+  on data_transaksi_layanan (id_layanan);
+
+create index data_transaksi_layanan_data_transaksi_master_id_master_fk
+  on data_transaksi_layanan (id_master);
+
 create index data_transaksi_data_kasir_id_kasir_fk
-  on data_transaksi (id_kasir);
+  on data_transaksi_master (id_kasir);
 
 create index data_transaksi_data_pelanggan_id_pelanggan_fk
-  on data_transaksi (id_pelanggan);
+  on data_transaksi_master (id_pelanggan);
+
+create table data_transaksi_sparepart
+(
+  id_transaksi int auto_increment
+    primary key,
+  id_master    int        not null,
+  id_part      varchar(5) not null,
+  jumlah       int        not null,
+  total_biaya  int        not null,
+  constraint data_transaksi_sparepart_data_transaksi_master_id_master_fk
+  foreign key (id_master) references data_transaksi_master (id_master),
+  constraint data_transaksi_sparepart_data_sparepart_id_part_fk
+  foreign key (id_part) references data_sparepart (id_part)
+);
+
+create index data_transaksi_sparepart_data_sparepart_id_part_fk
+  on data_transaksi_sparepart (id_part);
+
+create index data_transaksi_sparepart_data_transaksi_master_id_master_fk
+  on data_transaksi_sparepart (id_master);
 
 
