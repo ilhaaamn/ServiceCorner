8ac2f34c71373331ca1e754eb756150774da3a87
rafifrizqullah <mrafifr21@gmail.com>
2018-06-27 16:06:19 +0700

    add filter

--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -156,8 +156,11 @@
                 ?>
                 <div class="container-fluid p-4 ml-1 rounded mt-4" style="background-color: whitesmoke">
                     <h4>Tabel Transaksi</h4>
+                    <div class="mt-3 mb-3">
+                        <input type="text" class="form-control" id="cari" onkeyup="myFunction()" placeholder="Cari tanggal">
+                    </div>
                     <div class="table-wrapper-2">
-                        <table class="table table-bordered table-light" id="">
+                        <table class="table table-bordered table-light" id="tabel-transaksi">
                             <tr>
                                 <th style="width: 5%">No.</th>
                                 <th>Tanggal</th>
@@ -251,6 +254,29 @@
         // Configuration options go here
         options: {}
     });
+</script>
+
+<script>
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("cari");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tabel-transaksi");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 </script>
 
 </body>
